efe58239bfc6d49f083135c56698dca3901b0bf4
Dominik Bayer <dominikbayer@outlook.com>
2022-05-14 19:30:33 +0200

    'test'

--- js/pageChange.js
+++ js/pageChange.js
@@ -3,8 +3,10 @@ const nav1 = document.getElementById("nav1");
 const nav2 = document.getElementById("nav2");
 const nav3 = document.getElementById("nav3");
 const text = document.getElementById("nav-text");
+const navText = document.getElementById("nav-text-main");
 const aboutUs = document.getElementById("about-us");
 const landing = document.getElementById("landing-grid");
+const music = document.getElementById("music");
 
 var activeWindow = false;
 
@@ -44,6 +46,7 @@ async function openAboutUs() {
     nav1.style.width = "100vw";
     aboutUs.style.visibility = "visible";
     aboutUs.style.width = "100vw";
+    await sleep(700);
     aboutUs.style.opacity = "1";
 
     nav1.style.pointerEvents = "none";
@@ -78,6 +81,83 @@ async function closeAboutUs() {
     text.style.opacity = "1";
     text.style.display = "grid";
     nav2.style.display = "grid";
+    nav3.style.display = "grid";
+    title.style.display = "grid";
+    activeWindow = false;
+  }
+}
+
+async function openMusic() {
+  if (activeWindow == false) {
+    music.style.height = "100vh";
+    nav1.style.opacity = "0";
+    nav3.style.opacity = "0";
+    nav2.style = "border-top: 0px";
+    navText.style.visibility = "hidden";
+    navText.style.position = "absolute";
+    nav2.style.width = "0px";
+    title.style.opacity = "0";
+    text.style.opacity = "0";
+    await sleep(1200);
+    nav1.style.position = "absolute";
+    nav2.style.position = "absolute";
+    nav3.style.position = "absolute";
+    title.style.position = "absolute";
+    nav1.style.top = "0px";
+    nav2.style.top = "0px";
+    nav3.style.top = "0px";
+    title.style.top = "0px";
+    nav1.style.left = "0px";
+    nav2.style.left = "0px";
+    nav3.style.left = "0px";
+    title.style.left = "0px";
+
+    text.style.visibility = "hidden";
+    nav1.style.visibility = "hidden";
+    nav3.style.visibility = "hidden";
+    title.style.visibility = "hidden";
+
+    nav2.style = "border-top: 0px";
+    nav2.style.width = "100vw";
+    music.style.visibility = "visible";
+    music.style.width = "100vw";
+    await sleep(700);
+    music.style.opacity = "1";
+
+    nav2.style.pointerEvents = "none";
+    activeWindow = true;
+  }
+}
+
+async function closeMusic() {
+  if (activeWindow === true) {
+    console.log("test");
+    music.style.opacity = "0";
+    music.style.width = "0px";
+    music.style.visibility = "hidden";
+    nav2.style.width = "0px";
+    await sleep(1200);
+    nav1.style.position = "relative";
+    nav2.style.position = "relative";
+    nav3.style.position = "relative";
+    title.style.position = "relative";
+    nav1.style.visibility = "visible";
+    nav3.style.visibility = "visible";
+    title.style.visibility = "visible";
+    text.style.visibility = "visible";
+    text.style.opacity = "1";
+    navText.style.visibility = "visible";
+    navText.style.position = "relative";
+    nav2.style = "border-top-width: 20px";
+    music.style.height = "0px";
+    title.style.opacity = "1";
+    nav1.style.opacity = "1";
+    nav3.style.opacity = "1";
+    nav2.style.width = "100%";
+    nav2.style.opacity = "1";
+    text.style.opacity = "1";
+    text.style.display = "grid";
+    nav1.style.display = "grid";
     nav3.style.display = "grid";
     title.style.display = "grid";
     activeWindow = false;
